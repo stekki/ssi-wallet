a43953f785d23ff045deb60dd62ffc9268d509d2
stepank1 <thespammail8@gmail.com>
2023-11-15 22:24:43 +0200

    removed unused import (CI test push, should pass)

--- test/my_app_test.dart
+++ test/my_app_test.dart
@@ -1,5 +1,4 @@
 import 'package:flutter_test/flutter_test.dart';
-import 'package:flutter/material.dart'; // Replace with your widget
 import 'package:ssi_wallet/main.dart'; // Replace with your app's import
 
 void main() {
