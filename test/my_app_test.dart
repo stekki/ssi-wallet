03dd6e85d68f21320fb394c63ded3bf22378dc6b
stepank1 <thespammail8@gmail.com>
2023-11-15 20:40:17 +0200

    Added unit test for Hello, World! app

---
+++ test/my_app_test.dart
@@ -0,0 +1,10 @@
+import 'package:flutter_test/flutter_test.dart';
+import 'package:flutter/material.dart'; // Replace with your widget
+import 'package:ssi_wallet/main.dart'; // Replace with your app's import
+
+void main() {
+  testWidgets('MyWidget should display "Hello, World!"', (WidgetTester tester) async {
+    await tester.pumpWidget(const MainApp()); // Replace with your widget instance
+    expect(find.text('Hello, World!'), findsOneWidget);
+  });
+}
