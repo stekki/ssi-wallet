130a383b7b71a313a3cb8e194d09d956baae2d36
nkhieu02 <nguyenkhachieu1006@gmail.com>
2023-11-28 18:14:04 +0200

    add ignore, remove .idea

--- frontend/lib/custom_clippers.dart
+++ frontend/lib/custom_clippers.dart
@@ -4,56 +4,56 @@ class FirstClipper extends CustomClipper<Path> {
   @override
   Path getClip(Size size) {
     Path path = Path();
-    final double _xScaling = size.width / 414;
-    final double _yScaling = size.height / 896;
-    path.lineTo(-36.5944 * _xScaling, 16.2899 * _yScaling);
+    final double xScaling = size.width / 414;
+    final double yScaling = size.height / 896;
+    path.lineTo(-36.5944 * xScaling, 16.2899 * yScaling);
     path.cubicTo(
-      -163.713 * _xScaling,
-      -6.31618 * _yScaling,
-      -295 * _xScaling,
-      659.969 * _yScaling,
-      -295 * _xScaling,
-      220.935 * _yScaling,
+      -163.713 * xScaling,
+      -6.31618 * yScaling,
+      -295 * xScaling,
+      659.969 * yScaling,
+      -295 * xScaling,
+      220.935 * yScaling,
     );
     path.cubicTo(
-      -295 * _xScaling,
-      -218.1 * _yScaling,
-      -295 * _xScaling,
-      -106.259 * _yScaling,
-      -295 * _xScaling,
-      -106.259 * _yScaling,
+      -295 * xScaling,
+      -218.1 * yScaling,
+      -295 * xScaling,
+      -106.259 * yScaling,
+      -295 * xScaling,
+      -106.259 * yScaling,
     );
     path.cubicTo(
-      -295 * _xScaling,
-      -106.259 * _yScaling,
-      450 * _xScaling,
-      -106.259 * _yScaling,
-      450 * _xScaling,
-      -106.259 * _yScaling,
+      -295 * xScaling,
+      -106.259 * yScaling,
+      450 * xScaling,
+      -106.259 * yScaling,
+      450 * xScaling,
+      -106.259 * yScaling,
     );
     path.cubicTo(
-      450 * _xScaling,
-      -106.259 * _yScaling,
-      368.727 * _xScaling,
-      154.306 * _yScaling,
-      236.399 * _xScaling,
-      187.62 * _yScaling,
+      450 * xScaling,
+      -106.259 * yScaling,
+      368.727 * xScaling,
+      154.306 * yScaling,
+      236.399 * xScaling,
+      187.62 * yScaling,
     );
     path.cubicTo(
-      104.07 * _xScaling,
-      220.935 * _yScaling,
-      90.5244 * _xScaling,
-      38.896 * _yScaling,
-      -36.5944 * _xScaling,
-      16.2899 * _yScaling,
+      104.07 * xScaling,
+      220.935 * yScaling,
+      90.5244 * xScaling,
+      38.896 * yScaling,
+      -36.5944 * xScaling,
+      16.2899 * yScaling,
     );
     path.cubicTo(
-      -36.5944 * _xScaling,
-      16.2899 * _yScaling,
-      -36.5944 * _xScaling,
-      16.2899 * _yScaling,
-      -36.5944 * _xScaling,
-      16.2899 * _yScaling,
+      -36.5944 * xScaling,
+      16.2899 * yScaling,
+      -36.5944 * xScaling,
+      16.2899 * yScaling,
+      -36.5944 * xScaling,
+      16.2899 * yScaling,
     );
     return path;
   }
@@ -68,58 +68,58 @@ class SecondClipper extends CustomClipper<Path> {
   @override
   Path getClip(Size size) {
     Path path = Path();
-    final double _xScaling = size.width / 414;
-    final double _yScaling = size.height / 896;
+    final double xScaling = size.width / 414;
+    final double yScaling = size.height / 896;
     final offset = size.height * 0.4;
     path.moveTo(0, 100);
-    path.lineTo(0.353281 * _xScaling, 123.365 * _yScaling + offset);
+    path.lineTo(0.353281 * xScaling, 123.365 * yScaling + offset);
     path.cubicTo(
-      0.353281 * _xScaling,
-      123.365 * _yScaling + offset,
-      30.9078 * _xScaling,
-      197.16 * _yScaling + offset,
-      147.965 * _xScaling,
-      122.935 * _yScaling + offset,
+      0.353281 * xScaling,
+      123.365 * yScaling + offset,
+      30.9078 * xScaling,
+      197.16 * yScaling + offset,
+      147.965 * xScaling,
+      122.935 * yScaling + offset,
     );
     path.cubicTo(
-      265.021 * _xScaling,
-      48.7098 * _yScaling + offset,
-      430.351 * _xScaling,
-      122.111 * _yScaling + offset,
-      430.351 * _xScaling,
-      122.111 * _yScaling + offset,
+      265.021 * xScaling,
+      48.7098 * yScaling + offset,
+      430.351 * xScaling,
+      122.111 * yScaling + offset,
+      430.351 * xScaling,
+      122.111 * yScaling + offset,
     );
     path.cubicTo(
-      430.351 * _xScaling,
-      122.111 * _yScaling + offset,
-      429.998 * _xScaling,
-      0.99998 * _yScaling + offset,
-      429.998 * _xScaling,
-      0.99998 * _yScaling + offset,
+      430.351 * xScaling,
+      122.111 * yScaling + offset,
+      429.998 * xScaling,
+      0.99998 * yScaling + offset,
+      429.998 * xScaling,
+      0.99998 * yScaling + offset,
     );
     path.cubicTo(
-      429.998 * _xScaling,
-      0.99998 * _yScaling + offset,
-      0.0000430825 * _xScaling,
-      2.25413 * _yScaling + offset,
-      0.0000430825 * _xScaling,
-      2.25413 * _yScaling + offset,
+      429.998 * xScaling,
+      0.99998 * yScaling + offset,
+      0.0000430825 * xScaling,
+      2.25413 * yScaling + offset,
+      0.0000430825 * xScaling,
+      2.25413 * yScaling + offset,
     );
     path.cubicTo(
-      0.0000430825 * _xScaling,
-      2.25413 * _yScaling + offset,
-      0.353281 * _xScaling,
-      123.365 * _yScaling + offset,
-      0.353281 * _xScaling,
-      123.365 * _yScaling + offset,
+      0.0000430825 * xScaling,
+      2.25413 * yScaling + offset,
+      0.353281 * xScaling,
+      123.365 * yScaling + offset,
+      0.353281 * xScaling,
+      123.365 * yScaling + offset,
     );
     path.cubicTo(
-      0.353281 * _xScaling,
-      123.365 * _yScaling + offset,
-      0.353281 * _xScaling,
-      123.365 * _yScaling + offset,
-      0.353281 * _xScaling,
-      123.365 * _yScaling + offset,
+      0.353281 * xScaling,
+      123.365 * yScaling + offset,
+      0.353281 * xScaling,
+      123.365 * yScaling + offset,
+      0.353281 * xScaling,
+      123.365 * yScaling + offset,
     );
     return path;
   }
@@ -134,58 +134,58 @@ class ThirdClipper extends CustomClipper<Path> {
   @override
   Path getClip(Size size) {
     Path path = Path();
-    final double _xScaling = size.width / 414;
-    final double _yScaling = size.height / 896;
+    final double xScaling = size.width / 414;
+    final double yScaling = size.height / 896;
     final double offset = size.height * 0.9;
     path.moveTo(0, offset);
-    path.lineTo(0.0000305176 * _xScaling, 28.5155 * _yScaling + offset);
+    path.lineTo(0.0000305176 * xScaling, 28.5155 * yScaling + offset);
     path.cubicTo(
-      0.0000305176 * _xScaling,
-      28.5155 * _yScaling + offset,
-      30.3392 * _xScaling,
-      -33.3944 * _yScaling + offset,
-      147.612 * _xScaling,
-      28.5155 * _yScaling + offset,
+      0.0000305176 * xScaling,
+      28.5155 * yScaling + offset,
+      30.3392 * xScaling,
+      -33.3944 * yScaling + offset,
+      147.612 * xScaling,
+      28.5155 * yScaling + offset,
     );
     path.cubicTo(
-      264.885 * _xScaling,
-      90.4253 * _yScaling + offset,
-      430 * _xScaling,
-      28.5155 * _yScaling + offset,
-      430 * _xScaling,
-      28.5155 * _yScaling + offset,
+      264.885 * xScaling,
+      90.4253 * yScaling + offset,
+      430 * xScaling,
+      28.5155 * yScaling + offset,
+      430 * xScaling,
+      28.5155 * yScaling + offset,
     );
     path.cubicTo(
-      430 * _xScaling,
-      28.5155 * _yScaling + offset,
-      430 * _xScaling,
-      130 * _yScaling + offset,
-      430 * _xScaling,
-      130 * _yScaling + offset,
+      430 * xScaling,
+      28.5155 * yScaling + offset,
+      430 * xScaling,
+      130 * yScaling + offset,
+      430 * xScaling,
+      130 * yScaling + offset,
     );
     path.cubicTo(
-      430 * _xScaling,
-      130 * _yScaling + offset,
-      0.0000305176 * _xScaling,
-      130 * _yScaling + offset,
-      0.0000305176 * _xScaling,
-      130 * _yScaling + offset,
+      430 * xScaling,
+      130 * yScaling + offset,
+      0.0000305176 * xScaling,
+      130 * yScaling + offset,
+      0.0000305176 * xScaling,
+      130 * yScaling + offset,
     );
     path.cubicTo(
-      0.0000305176 * _xScaling,
-      130 * _yScaling + offset,
-      0.0000305176 * _xScaling,
-      28.5155 * _yScaling + offset,
-      0.0000305176 * _xScaling,
-      28.5155 * _yScaling + offset,
+      0.0000305176 * xScaling,
+      130 * yScaling + offset,
+      0.0000305176 * xScaling,
+      28.5155 * yScaling + offset,
+      0.0000305176 * xScaling,
+      28.5155 * yScaling + offset,
     );
     path.cubicTo(
-      0.0000305176 * _xScaling,
-      28.5155 * _yScaling + offset,
-      0.0000305176 * _xScaling,
-      28.5155 * _yScaling + offset,
-      0.0000305176 * _xScaling,
-      28.5155 * _yScaling + offset,
+      0.0000305176 * xScaling,
+      28.5155 * yScaling + offset,
+      0.0000305176 * xScaling,
+      28.5155 * yScaling + offset,
+      0.0000305176 * xScaling,
+      28.5155 * yScaling + offset,
     );
     return path;
   }
