9cdbfb2466f321ccedb94f247e061fc9330ecb16
markrenssi <mark.renssi@aalto.fi>
2024-01-23 18:58:27 +0200

    added routing mechanism with go_router

---
+++ frontend/lib/routes/routes.dart
@@ -0,0 +1,42 @@
+import 'package:go_router/go_router.dart';
+import '../screens/landing_screen.dart';
+import '../screens/login_screen.dart';
+import '../screens/register_screen.dart';
+import '../screens/home_screen.dart';
+import '../screens/chat_screen.dart';
+import '../screens/credential_screen.dart';
+
+final router = GoRouter(
+  routes: [
+    GoRoute(
+      name: 'landing',
+      path: '/',
+      builder: (context, state) => const LandingScreen(),
+    ),
+    GoRoute(
+      name: 'register',
+      path: '/register',
+      builder: (context, state) => const RegisterScreen(),
+    ),
+    GoRoute(
+      name: 'login',
+      path: '/login',
+      builder: (context, state) => const LoginScreen(),
+    ),
+    GoRoute(
+      name: 'home',
+      path: '/home',
+      builder: (context, state) => const HomeScreen(),
+    ),
+    GoRoute(
+      name: 'chat',
+      path: '/chat',
+      builder: (context, state) => const ChatScreen(),
+    ),
+    GoRoute(
+      name: 'credential',
+      path: '/credential',
+      builder: (context, state) => const CredentialScreen(),
+    ),
+  ],
+);
