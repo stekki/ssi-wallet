db96ed9be56e043620d1d46995ac71a9cecdea43
Joonas Koivu <koivu.joonas@gmail.com>
2024-02-22 17:24:35 +0200

    Changed provider to use real data for connections

--- frontend/lib/providers/providers.dart
+++ frontend/lib/providers/providers.dart
@@ -1,4 +1,5 @@
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:frontend/services/connection_service.dart';
 import 'package:frontend/widgets/credential.dart';
 import 'package:graphql_flutter/graphql_flutter.dart';
 import '../services/graphql_service.dart';
@@ -16,15 +17,7 @@ void getConnections() async {
   }
 }
 
-final connectionsFutureProvider = FutureProvider<List<Connection>>(
-  (ref) async {
-    getConnections();
-    return await Future.delayed(
-      const Duration(seconds: 1),
-      () => gqlConnections,
-    );
-  },
-);
+final connectionServiceProvider = Provider((ref) => ConnectionService());
 
 final credentialsFutureProvider = FutureProvider<List<Credential>>(
   (ref) async {
