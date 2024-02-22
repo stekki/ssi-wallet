83f0dd7f1cb6494b14aa6b0567aaa0dfaad3d339
Joonas Koivu <koivu.joonas@gmail.com>
2024-02-22 17:20:57 +0200

    Changed homescreen to render real data

--- frontend/lib/screens/home_screen.dart
+++ frontend/lib/screens/home_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:frontend/services/connection_service.dart';
 
 import 'package:frontend/utils/styles.dart';
 import 'package:frontend/widgets/connection_card.dart';
@@ -8,7 +9,7 @@ import 'package:frontend/screens/loading_screen.dart';
 import '../Models/models.dart';
 
 class HomeScreen extends ConsumerStatefulWidget {
-  const HomeScreen({super.key});
+  const HomeScreen({Key? key}) : super(key: key);
 
   @override
   ConsumerState<HomeScreen> createState() => _HomeScreenState();
@@ -34,150 +35,31 @@ class _HomeScreenState extends ConsumerState<HomeScreen>
 
   @override
   Widget build(BuildContext context) {
-    final connectionsFuture = ref.watch(connectionsFutureProvider);
+    final AsyncValue<List<Connection>> connectionsAsyncValue =
+        ref.watch(connectionsFutureProvider);
     final height = MediaQuery.of(context).size.height;
-    if (connectionsFuture.isLoading) {
-      return const LoadingScreen();
-    }
+
     return Scaffold(
       body: Container(
         decoration: scaffoldBackground,
-        child: connectionsFuture.when(
-          loading: () => const Text(
-            "Loading...",
-          ),
-          error: (err, stack) => const Text(
-            "Error loading chats",
-          ),
+        child: connectionsAsyncValue.when(
+          loading: () => const LoadingScreen(),
+          error: (error, stackTrace) => Text("Error: $error"),
           data: (connections) => Column(
             children: [
               TabBar(
                 controller: _tabController,
                 tabs: const [
                   Tab(text: 'Open chats'),
-                  Tab(text: 'chat request (0)'),
+                  Tab(text: 'Chat requests (0)'),
                 ],
               ),
               Expanded(
                 child: TabBarView(
                   controller: _tabController,
                   children: [
-                    CustomScrollView(
-                      slivers: <Widget>[
-                        SliverPadding(
-                          padding: const EdgeInsets.fromLTRB(20, 0, 20, 20),
-                          sliver: SliverAppBar(
-                            shape: const ContinuousRectangleBorder(
-                              borderRadius: BorderRadius.only(
-                                bottomLeft: Radius.circular(30),
-                                bottomRight: Radius.circular(30),
-                              ),
-                            ),
-                            backgroundColor:
-                                const Color.fromRGBO(255, 255, 255, 1),
-                            pinned: true,
-                            floating: true,
-                            title: TextField(
-                              keyboardType: TextInputType.multiline,
-                              controller: _searchController,
-                              decoration: const InputDecoration(
-                                hintText: 'Search connection',
-                                prefixIcon: Icon(Icons.search),
-                                border: InputBorder.none,
-                              ),
-                              onChanged: (value) {
-                                setState(
-                                    () => filterValue = value.toLowerCase());
-                              },
-                            ),
-                          ),
-                        ),
-                        SliverList(
-                          delegate: SliverChildBuilderDelegate(
-                            (BuildContext context, int index) {
-                              return SizedBox(
-                                height: height * 0.75,
-                                child: Column(
-                                  children: [
-                                    Expanded(
-                                      child: ListView(
-                                        children: connections
-                                            .where((connection) => connection
-                                                .theirLabel
-                                                .toLowerCase()
-                                                .contains(filterValue))
-                                            .map((connection) => ConnectionCard(
-                                                name: connection.theirLabel))
-                                            .toList(),
-                                      ),
-                                    ),
-                                  ],
-                                ),
-                              );
-                            },
-                            childCount: 1,
-                          ),
-                        ),
-                      ],
-                    ),
-                    CustomScrollView(
-                      slivers: <Widget>[
-                        SliverPadding(
-                          padding: const EdgeInsets.fromLTRB(20, 0, 20, 20),
-                          sliver: SliverAppBar(
-                            shape: const ContinuousRectangleBorder(
-                              borderRadius: BorderRadius.only(
-                                bottomLeft: Radius.circular(30),
-                                bottomRight: Radius.circular(30),
-                              ),
-                            ),
-                            backgroundColor:
-                                const Color.fromRGBO(255, 255, 255, 1),
-                            pinned: true,
-                            floating: true,
-                            title: TextField(
-                              keyboardType: TextInputType.multiline,
-                              controller: _searchController,
-                              decoration: const InputDecoration(
-                                hintText: 'Search connection',
-                                prefixIcon: Icon(Icons.search),
-                                border: InputBorder.none,
-                              ),
-                              onChanged: (value) {
-                                setState(
-                                    () => filterValue = value.toLowerCase());
-                              },
-                            ),
-                          ),
-                        ),
-                        SliverList(
-                          delegate: SliverChildBuilderDelegate(
-                            (BuildContext context, int index) {
-                              return SizedBox(
-                                height: height * 0.75,
-                                child: Column(
-                                  children: [
-                                    Expanded(
-                                      child: ListView(
-                                        children: connections
-                                            .where((connection) => connection
-                                                .theirLabel
-                                                .toLowerCase()
-                                                .contains(filterValue))
-                                            .map((c) => ConnectionCard(
-                                                name: c.theirLabel))
-                                            .toList(),
-                                      ),
-                                    ),
-                                  ],
-                                ),
-                              );
-                            },
-                            childCount: 1,
-                          ),
-                        ),
-                      ],
-                    )
+                    _buildConnectionListView(context, connections),
+                    _buildConnectionListView(context, connections),
                   ],
                 ),
               ),
@@ -185,6 +67,58 @@ class _HomeScreenState extends ConsumerState<HomeScreen>
           ),
         ),
       ),
+    );
+  }
+
+  Widget _buildConnectionListView(
+      BuildContext context, List<Connection> connections) {
+    return CustomScrollView(
+      slivers: <Widget>[
+        SliverPadding(
+          padding: const EdgeInsets.fromLTRB(20, 0, 20, 20),
+          sliver: SliverAppBar(
+            shape: const ContinuousRectangleBorder(
+              borderRadius: BorderRadius.only(
+                bottomLeft: Radius.circular(30),
+                bottomRight: Radius.circular(30),
+              ),
+            ),
+            backgroundColor: const Color.fromRGBO(255, 255, 255, 1),
+            pinned: true,
+            floating: true,
+            title: TextField(
+              keyboardType: TextInputType.multiline,
+              controller: _searchController,
+              decoration: const InputDecoration(
+                hintText: 'Search connection',
+                prefixIcon: Icon(Icons.search),
+                border: InputBorder.none,
+              ),
+              onChanged: (value) {
+                setState(() => filterValue = value.toLowerCase());
+              },
+            ),
+          ),
+        ),
+        SliverList(
+          delegate: SliverChildBuilderDelegate(
+            (BuildContext context, int index) {
+              return SizedBox(
+                height: MediaQuery.of(context).size.height * 0.75,
+                child: ListView(
+                  children: connections
+                      .where((connection) => connection.theirLabel
+                          .toLowerCase()
+                          .contains(filterValue))
+                      .map((c) => ConnectionCard(connection: c))
+                      .toList(),
+                ),
+              );
+            },
+            childCount: 1,
+          ),
+        ),
+      ],
     );
   }
 }
